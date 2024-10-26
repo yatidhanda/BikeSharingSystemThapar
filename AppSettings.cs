701b847cebba1889e0e618e9c8ce7105295b47c0
yatidhanda <100429679+yatidhanda@users.noreply.github.com>
2024-10-26 14:10:30 +0530

    Add files via upload

---
+++ AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeSharingApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "https://bikesharingsystemtiet-bhc6acbabsfmcxhm.eastasia-01.azurewebsites.net/";
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BikeSharingApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
